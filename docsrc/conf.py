c579cea74920298fcdde17818a4203ead5a96e8d
iPad Guy <ipad.guy.100@gmx.com>
2019-10-29 12:52:37 -0400

    Hack 1

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -13,7 +13,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+# At the top.
 
+import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -47,7 +49,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
